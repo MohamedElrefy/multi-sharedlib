706b97558b73f68df63cccc1404b801c0c84a7a5
mohamed_elrefy <elrefymohamed2002@gmail.com>
2025-11-06 06:24:54 +0200

    updatee

--- vars/updateDeploymentYaml.groovy
+++ vars/updateDeploymentYaml.groovy
@@ -1,9 +1,19 @@
-def call(String projectDir, String fileName, String imageName, String buildNumber) {
+def call(String projectDir, String fileName, String imageName, String buildNumber, String branchName) {
     stage('Update deployment.yaml') {
         dir(projectDir) {
             sh """
+              
                 sed -i 's|image: .*|image: ${imageName}:${buildNumber}|g' ${fileName}
-                echo "Updated deployment file:"
+
+               
+                if grep -q 'namespace:' ${fileName}; then
+                    sed -i 's|namespace: .*|namespace: ${branchName}|g' ${fileName}
+                else
+                   
+                    awk -v ns="${branchName}" '/metadata:/{print; print "  namespace: " ns; next}1' ${fileName} > tmp && mv tmp ${fileName}
+                fi
+
+                echo "✅ Updated deployment file:"
                 cat ${fileName}
             """
         }
